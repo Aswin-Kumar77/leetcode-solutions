ea6906181cf6f71a961e3040f0263e543a2f0298
Aswin-Kumar77 <aswin6425003@gmail.com>
2026-09-05 22:45:39 +0530

    Add best time to buy and sell stock solution

---
+++ 121-Buy-Sell-Stock/src/main.java
@@ -0,0 +1,19 @@
+class main
+{
+    public int maxProfit(int[] prices)
+    {
+        int maxprofit = 0;
+        for(int i = 0; i < prices.length; i++)
+        {
+            for(int j = i+1; j< prices.length; j++)
+            {
+                int profit = prices[j] - prices[i];
+                if (profit>maxprofit)
+                {
+                    maxprofit = profit;
+                }
+            }
+        }
+        return maxprofit;
+    }
+}
